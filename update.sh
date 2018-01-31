ae511bd425fcfa51bae48261c77c5894030a4889
e155721 <e155721+github@ie.u-ryukyu.ac.jp>
2018-02-01 00:38:27 +0900

    [update] Update update.sh

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
 readonly PROFILE='.zpf_local'
 readonly CONFIG='.zshrc'
+readonly HISTORY='.zsh_history'
 
 cd "$HOME"
 [ -e "$PROFILE" ] && mv -v $PROFILE .zprofile_local
 [ -e "$CONFIG" ] && mv -v $CONFIG .zsh_local
+[ -e "$HISTORY" ] && mv -v $HISTORY .zhistory
 hh
